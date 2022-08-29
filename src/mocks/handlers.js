0cc2e24d7f5bb138e5a44411e47f73c196ab52e4
Prakash Shankar <56201146+prakashankar39@users.noreply.github.com>
2022-08-29 00:37:07 -0400

    fix code to pass all the tests

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,9 +6,6 @@ export const handlers = [
       ctx.json([
         { name: "Chocolate", imagePath: "/images/chocolate.png" },
         { name: "Vanilla", imagePath: "/images/vanilla.png" },
-        { name: "Cherries", imagePath: "/images/cherries.png" },
-        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
-        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
       ])
     );
   }),
--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,6 @@
 import Col from "react-bootstrap/Col";
 
-export default function ToppingOption(name, imagePath) {
+export default function ToppingOption({ name, imagePath }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
